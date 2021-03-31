6c1fd1336e961e8a84a7630e6d78894cbfba67de
Kirill Frolov <frolov.cyrill@gmail.com>
2021-03-31 18:18:06 +0300

    Sorting tickets list

--- src/components/TicketsList.jsx
+++ src/components/TicketsList.jsx
@@ -4,16 +4,19 @@ import React from 'react'
 import Ticket from './Ticket'
 import { connect } from 'react-redux'
 
-const TicketsList = ({ tickets, stopsFilter }) => {
+const TicketsList = ({ tickets, stopsFilter, orderBy }) => {
     const filterBySteps = ticket => {
         if (stopsFilter.length === 0) return true
         return ticket.segments.reduce((sum, segment) => stopsFilter.includes(segment.stops.length) && sum, true)
     }
 
+    const comparator = (one, two) => one[orderBy] - two[orderBy]
+
     return (
         <div className='tickets-list'>
             {tickets
                 .filter(filterBySteps)
+                .sort(comparator)
                 .slice(0, 20)
                 .map(ticket => <Ticket {...ticket} key={JSON.stringify(ticket)} />)}
         </div>
@@ -23,6 +26,7 @@ const TicketsList = ({ tickets, stopsFilter }) => {
 const mapStateToProps = state => ({
     tickets: state.tickets,
     stopsFilter: state.stopsFilter,
+    orderBy: state.orderBy,
 })
 
 export default connect(mapStateToProps)(TicketsList)
