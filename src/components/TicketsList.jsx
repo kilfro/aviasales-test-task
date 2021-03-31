3056f99dedd4a84b2cf4bc69558b1eb45b0d3a40
Kirill Frolov <frolov.cyrill@gmail.com>
2021-03-31 16:48:41 +0300

    Filter tickets in TicketsList

--- src/components/TicketsList.jsx
+++ src/components/TicketsList.jsx
@@ -4,14 +4,24 @@ import React from 'react'
 import Ticket from './Ticket'
 import { connect } from 'react-redux'
 
-const TicketsList = ({ tickets }) => (
-    <div className='tickets-list'>
-        {tickets.slice(0, 20).map(ticket => <Ticket {...ticket} key={JSON.stringify(ticket)} />)}
-    </div>
-)
+const TicketsList = ({ tickets, stopsFilter }) => {
+    const filterBySteps = ticket => (
+        ticket.segments.reduce((sum, segment) => segment.stops.length === stopsFilter && sum, true)
+    )
+
+    return (
+        <div className='tickets-list'>
+            {tickets
+                .filter(filterBySteps)
+                .slice(0, 20)
+                .map(ticket => <Ticket {...ticket} key={JSON.stringify(ticket)} />)}
+        </div>
+    )
+}
 
 const mapStateToProps = state => ({
     tickets: state.tickets,
+    stopsFilter: state.stopsFilter,
 })
 
 export default connect(mapStateToProps)(TicketsList)
